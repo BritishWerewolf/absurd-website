047d3a32757198f57f6b7001a8344d1f374fc967
BritishWerewolf <britishwerewolfdev@gmail.com>
2024-09-15 15:36:08 +0100

    Installed a slightly modified Astrolus theme. https://www.tailwindawesome.com/resources/astrolus

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['variant', [
+		'@media (prefers-color-scheme: dark) { &:not(.light *) }',
+		'&:is(.dark *)',
+	]],
 	theme: {
 		extend: {},
 	},
